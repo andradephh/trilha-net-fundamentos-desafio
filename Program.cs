c5d46ed7df7ad3d4820e8ff53fa9f103c3491de2
andradephh <150219294+andradephh@users.noreply.github.com>
2023-11-10 22:37:53 -0300

    Add files via upload
    projeto estacionamento trilha-net-fundamentos desafio

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioEstacionamento.Models;
+using DesafioEstacionamento.Services;
+
+namespace DesafionEstacionamento
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var service = new EstacionamentoService(new Estacionamento());
+
+            service.IniciarEstacionamento();
+        }
+    }
+}
